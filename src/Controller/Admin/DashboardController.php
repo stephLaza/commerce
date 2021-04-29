6a9c08717ecd3f96b5942967f821e5da2e893f65
stepbyone <ae.stepbyone@gmail.com>
2021-04-29 14:32:17 +0200

    tunel d'achat suite

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Entity\Order;
 use App\Entity\Carrier;
 use App\Entity\Product;
 use App\Entity\Category;
@@ -10,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
@@ -19,7 +21,10 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+        $routeBuilder = $this->get(AdminUrlGenerator::class);
+
+        return $this->redirect($routeBuilder->setController(OrderCrudController::class)->generateUrl());
     }
 
     public function configureDashboard(): Dashboard
@@ -32,8 +37,10 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Utilisateur', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Commande', 'fas fa-shopping-cart', Order::class);
         yield MenuItem::linkToCrud('Categorie', 'fas fa-list', Category::class);
         yield MenuItem::linkToCrud('Produit', 'fas fa-list', Product::class);
         yield MenuItem::linkToCrud('Livraison', 'fas fa-truck', Carrier::class);
+
     }
 }
--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,58 +2,102 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Classe\Cart;
+use App\Entity\Order;
 use App\Form\OrderType;
+use App\Entity\OrderDetails;
+
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class OrderController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/commande", name="order")
      */
     public function index(Cart $cart, Request $request)
     {
-        if (!$this->getUser()->getAddresses()->getValues())
-        {
+        if (!$this->getUser()->getAddresses()->getValues()) {
             return $this->redirectToRoute('add_address');
         }
 
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
-        
+
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
-            'cart' => $cart->getFull() 
+            'cart' => $cart->getFull()
         ]);
     }
 
     /**
-     * @Route("/commande/recapitulatif", name="order_recap")
+     * @Route("/commande/recapitulatif", name="order_recap", methods={"POST"})
      */
     public function add(Cart $cart, Request $request)
     {
-       
+
 
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            dd($form->getData());
-            // $user = $form->getData();
-            // $password = $encoder->encodePassword($user, $user->getPassword());
-            // $user->setPassword($password);
-            // $this->entityManager->persist($user);
-            // $this->entityManager->flush();
-            // return $this->redirectToRoute("account");
+
+            $date = new DateTime();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+            $delivery_content = $delivery->getFirstname() . ' ' . $delivery->getlastname();
+            $delivery_content .= '<br/>' . $delivery->getPhone();
+
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>' . $delivery->getCompany();
+            }
+            $delivery_content .= '<br/>' . $delivery->getAddress();
+            $delivery_content .= '<br/>' . $delivery->getPostal() . ' ' . $delivery->getCity();
+
+            $delivery_content .= '<br/>' . $delivery->getCountry();
+
+            $order = new Order();
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+            $this->entityManager->persist($order);
+            //enregistre mes produits orderDetails()
+
+            foreach ($cart->getFull() as $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product['product']->getName());
+                $orderDetails->setQuantity($product['quantity']);
+                $orderDetails->setPrice($product['product']->getPrice());
+                $orderDetails->setTotal($product['product']->getPrice() * $product['quantity']);
+                $this->entityManager->persist($orderDetails);
+            }
+            $this->entityManager->flush();
+            return $this->render('order/add.html.twig', [
+
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
         }
-        return $this->render('order/index.html.twig', [
-           
-            'cart' => $cart->getFull() 
-        ]);
+        return $this->redirectToRoute('cart');
+       
     }
 }
